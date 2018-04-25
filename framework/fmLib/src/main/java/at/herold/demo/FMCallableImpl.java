bac9a6ee8a6ee98d97ece865a1cdb6c5a8ec086d
Cristian-Petrut Petrache <cristianpetrache@gmail.com>
2018-04-25 11:16:43 +0200

    Task-8: added some debug logging message

--- framework/fmLib/src/main/java/at/herold/demo/FMCallableImpl.java
+++ framework/fmLib/src/main/java/at/herold/demo/FMCallableImpl.java
@@ -1,13 +1,18 @@
 package at.herold.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class FMCallableImpl implements FMCallable
 {
+   private static final Logger LOGGER = LogManager.getLogger(FMCallableImpl.class);
 
    private static final String FINGERPRINT = "fmLib - at.herold.demo.FMCallableImpl: ";
 
    @Override
    public String call(String message)
    {
+      LOGGER.debug("at.herold.demo.FMCallableImpl.call: " + message);
       return FINGERPRINT + message;
    }
 }
